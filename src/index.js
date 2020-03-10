b0cc5a8719d6e4a8e7ee61da094bf1db9778ee04
jendos-kgd <edanileyko@gmail.com>
2020-03-10 17:31:17 +0200

    Configure webpack and plug fonts Quicksand

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as $  from 'jquery'
+import './styles/style.css'
+import Vector from './assets/Vector.png'
+
+let a = 7
+let b = 10
+let c = 12
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,51 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/.index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'], //Какие расширения понимать, когда они не указаны
+    alias: {
+      '@': path.resolve(__dirname, 'src') //Сокращение написания пути к файлам и папкам
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|svg|woff)$/,
+        use: ['file-loader']
+      }
+    ]
   }
 }
